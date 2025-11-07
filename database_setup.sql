6b91c55f636b6b1df13f0d298771f2888c968343
D-Faison <dfaison2017@gmail.com>
2025-11-07 17:00:35 -0500

    Update database_setup.sql

--- database_setup.sql
+++ database_setup.sql
@@ -8,3 +8,19 @@ USE moffatbay;
 CREATE USER IF NOT EXISTS 'PLACEHOLDER_USER'@'localhost' IDENTIFIED BY 'PLACEHOLDER_PASSWORD';
 GRANT ALL PRIVILEGES ON moffatbay.* TO 'PLACEHOLDER_USER'@'localhost';
 FLUSH PRIVILEGES;
+
+
+-- Billing Method Table
+DROP TABLE IF EXISTS billing_methods;
+CREATE TABLE billing_methods(
+	billingID int primary key auto_increment,
+    customer_id int not null,
+    type ENUM('Paypal','Apple Pay','Cash App','Credit Card','Shop','Other') not null, -- Fixed Payment methods
+    name varchar(48)
+	
+  CONSTRAINT fk_billing_customer
+		FOREIGN KEY (customer_id) REFERENCES customers(customerId)
+        -- If customer updates or deletes information automatically update and delete
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
